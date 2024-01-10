5dd4abf39472d732e9d8b05a3107fe3392e497c1
sc <sc@DESKTOP-I52LQ55>
2024-01-10 16:07:32 +0530

    first commit

---
+++ src/main/java/project1/project01/App.java
@@ -0,0 +1,14 @@
+package project1.project01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Application Started.. " );
+    }
+}
+
---
+++ src/test/java/project1/project01/AppTest.java
@@ -0,0 +1,20 @@
+package project1.project01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+   
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	System.out.println("testing started.. ");
+        assertTrue( true );
+        
+    }
+}
